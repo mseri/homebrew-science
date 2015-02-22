0704ab70f2a3e1c2c0b9c27d7a7a0f889a609daf
Marcello Seri <marcello.seri@gmail.com>
2015-02-22 12:12:58 +0000

    Formula for Arb (Fast C library for arbitrary-precision numerics with rigorous error bounds - http://fredrikj.net/arb/)

---
+++ arb.rb
@@ -0,0 +1,62 @@
+require "formula"
+
+# TODOs: 
+# - Add Arb-FLINT integration in flint formula, setting Arb optional
+# - (?) Add possibility to choose MPIR in place of GMP
+
+class Arb < Formula
+  homepage "http://fredrikj.net/arb/index.html"
+  #tag "math"
+  url "https://github.com/fredrik-johansson/arb/archive/2.5.0.tar.gz"
+  sha1 "ba51573e0c50250bbb16bfca027dc26531347f12"
+  head "https://github.com/fredrik-johansson/arb.git", :branch => "trunk"
+
+  bottle do
+    root_url "https://downloads.sf.net/project/machomebrew/Bottles/science"
+    sha1 "617731bca8e304ff9351c0fbee22ac078bbeee5e" => :yosemite
+    sha1 "ec020b7499a3b7d19fa4a2c44d2abc30d34c21aa" => :mavericks
+    sha1 "29dbbfc5cf472a0922376ec47e40975b9caec6bb" => :mountain_lion
+  end
+
+  depends_on "gmp"
+  depends_on "mpfr"
+  depends_on "flint"
+
+  # Will be enabled once the new stable (with patched tests) is released
+  # some of the tests in 2.5.0 are broken because they call not yet implemented
+  # methods
+  #option "with-check", "Enable build-time checking (not recommended)"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    # We need to remove this line to have 2.5.0 compiled on OSX
+    # it is fixed in the new version, this line will disappear then
+    system "sed -i.brew.orig 's/$(QUIET_AR) $(AR) rcs libarb.a $(OBJS);//' Makefile"
+    system "make"
+    # Will be enabled once the new stable (with patched tests) is released
+    # see above
+    #system "make", "check" if build.with? "check"
+    system "make", "install"
+  end
+
+  test do
+    (testpath / "test.c").write <<-EOS.undent
+      #include <stdio.h>
+      #include <arb.h>
+
+      int main()
+      {
+        arb_t x;
+        arb_init(x);
+        arb_const_pi(x, 50 * 3.33);
+        arb_printn(x, 50, 0); printf("\n");
+        printf("Computed with arb-%s\n", arb_version);
+        arb_clear(x);
+
+        return EXIT_SUCCESS;
+      }
+    EOS
+    system ENV.cc, "test.c", "-larb", "-lflint", "-I/usr/local/include/flint", "-o", "test"
+    system "./test"
+  end
+end
